dfd6507c068607368f3468f64b8266e4d1da202f
john-best1 <besty_2006@hotmail.co.uk>
2018-12-23 12:19:24 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
+import { FormBuilder } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms'
+import { MatToolbarModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatIconModule, MatButtonModule, MatCardModule, MatTableModule, MatDividerModule, MatSnackBarModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TokenComponent } from './token/token.component';
+import { TestOptionsComponent } from './test-options/test-options.component';
+import { StaffInvoiceListComponent } from './staff-invoice-list/staff-invoice-list.component';
+import { CustomerInvoiceViewComponent } from './customer-invoice-view/customer-invoice-view.component';
+import { StaffInvoiceViewComponent } from './staff-invoice-view/staff-invoice-view.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  { path: 'token', component: TokenComponent},
+  { path: 'test', component: TestOptionsComponent},
+  { path: 'invoices', component: StaffInvoiceListComponent},
+  { path: 'customerInvoice', component: CustomerInvoiceViewComponent},
+  { path: 'saveInvoice', component: StaffInvoiceViewComponent},
+  { path: '', redirectTo: 'token', pathMatch: 'full'},
+  { path: '**', component: NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TokenComponent,
+    TestOptionsComponent,
+    StaffInvoiceListComponent,
+    CustomerInvoiceViewComponent,
+    StaffInvoiceViewComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    MatInputModule, 
+    MatSelectModule, 
+    MatOptionModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    MatCardModule, 
+    MatTableModule, 
+    MatDividerModule, 
+    MatSnackBarModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
